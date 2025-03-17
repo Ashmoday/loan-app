6ae11ad06aecd92075c8da106ea4f1c71d7bd985
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-17 17:06:57 -0300

    base entity added

---
+++ src/main/java/com/ashmoday/bets/common/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.ashmoday.bets.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updated_at;
+}
