9e67c3bf14cddc66f4cb0eaa47baf245e504a8be
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-17 17:05:35 -0300

    version 0.0

---
+++ src/test/java/com/ashmoday/bets/BetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashmoday.bets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
