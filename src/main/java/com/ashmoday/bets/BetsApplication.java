36bf62f56dcd625635f54be528719967edb19e78
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-17 18:54:35 -0300

    Fixed typos and added EntityListener

--- src/main/java/com/ashmoday/bets/BetsApplication.java
+++ src/main/java/com/ashmoday/bets/BetsApplication.java
@@ -2,8 +2,10 @@ package com.ashmoday.bets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BetsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ashmoday/bets/common/BaseEntity.java
+++ src/main/java/com/ashmoday/bets/common/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.ashmoday.bets.common;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -20,6 +18,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue
@@ -27,8 +26,8 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
     @LastModifiedDate
     @Column(insertable = false)
-    private LocalDateTime updated_at;
+    private LocalDateTime updatedAt;
 }
