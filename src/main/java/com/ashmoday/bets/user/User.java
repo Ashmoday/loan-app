9e4bc480ad1c05fa6097bb1e76e139a66bffdc33
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-19 12:39:26 -0300

    Changes on user ID

--- src/main/java/com/ashmoday/bets/user/User.java
+++ src/main/java/com/ashmoday/bets/user/User.java
@@ -1,6 +1,7 @@
 package com.ashmoday.bets.user;
 
 import com.ashmoday.bets.character.Character;
+import com.ashmoday.bets.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,7 +22,8 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_sequence", allocationSize = 1)
     private Integer id;
     @Column(unique = true, name = "ucp_id", nullable = false)
     private Integer ucpId;
@@ -35,4 +38,7 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Character> characters;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 }
