7ea222e2518d080d4505eface13eb2fa2737263c
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-19 23:51:04 -0300

    Character request & Repository created

---
+++ src/main/java/com/ashmoday/bets/character/CharacterRepository.java
@@ -0,0 +1,6 @@
+package com.ashmoday.bets.character;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
+}
