5600ade0ae4b285bf1ae98e25a79f1923ab8541c
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-24 21:38:14 -0300

    Login added to authenticationService

--- src/main/java/com/ashmoday/bets/auth/AuthenticationController.java
+++ src/main/java/com/ashmoday/bets/auth/AuthenticationController.java
@@ -1,23 +1,35 @@
 package com.ashmoday.bets.auth;
 
+
+import com.ashmoday.bets.handler.GlobalExceptionHandler;
+import com.ashmoday.bets.user.User;
+import com.ashmoday.bets.user.UserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    private final AuthenticationService authenticationService;
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> register(
-            @RequestBody @Valid RegistrationRequest request
-    ){
-        authenticationService.register(request);
-        return ResponseEntity.accepted().build();
-    }
+    private final AuthenticationService service;
 
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody @Valid AuthenticationRequest request
+    )
+    {
+        return ResponseEntity.ok(service.login(request));
+    }
 }
