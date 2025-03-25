bcb7a43a8af0151a9269db9d6308beabc1540805
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-25 15:00:53 -0300

    Added register to AuthController

--- src/main/java/com/ashmoday/bets/auth/AuthenticationController.java
+++ src/main/java/com/ashmoday/bets/auth/AuthenticationController.java
@@ -1,22 +1,15 @@
 package com.ashmoday.bets.auth;
 
 
-import com.ashmoday.bets.handler.GlobalExceptionHandler;
-import com.ashmoday.bets.user.User;
-import com.ashmoday.bets.user.UserRepository;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 
 @RestController
@@ -31,5 +24,13 @@ public class AuthenticationController {
     )
     {
         return ResponseEntity.ok(service.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
     }
 }
