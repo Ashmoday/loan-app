bad0c3a9fef10424fe01e402b1c9fecf90d65a7c
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-27 09:36:57 -0300

    Added Loan class

---
+++ src/main/java/com/ashmoday/loans/loan/Loan.java
@@ -0,0 +1,33 @@
+package com.ashmoday.loans.loan;
+
+import com.ashmoday.loans.character.Character;
+import com.ashmoday.loans.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loan extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name="character_id", nullable = false)
+    private Character character;
+    @Column(nullable = false)
+    private Integer amount;
+    @Column(nullable = false)
+    private Integer weeks;
+    @Column(nullable = false)
+    private Double interestRate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LoanStatus status = LoanStatus.PENDING;
+
+}
