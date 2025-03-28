1fa7dd53e8e802bb0b281b9d0296a7457222e6ae
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-28 13:51:02 -0300

    New queries added to LoanRepository

--- src/main/java/com/ashmoday/loans/loan/LoanRepository.java
+++ src/main/java/com/ashmoday/loans/loan/LoanRepository.java
@@ -1,12 +1,18 @@
 package com.ashmoday.loans.loan;
 
 import com.ashmoday.loans.character.Character;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LoanRepository extends JpaRepository<Loan, Integer>, JpaSpecificationExecutor<Loan> {
     List<Loan> findByCharacterAndStatus(Character character, LoanStatus status);
     List<Loan> findByCharacter(Character character);
+    Page<Loan> findByStatus(LoanStatus status, Pageable pageable);
+    Optional<Loan> findByIdAndCharacter(Integer loanId, Character character);
+    Optional<Loan> findByIdAndStatus(Integer loanId, LoanStatus status);
 }
