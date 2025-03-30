b26e6fcc712b5e0db7884c6cd3b193c6d2b85065
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-03-30 18:55:54 -0300

    Typo fix

--- src/main/java/com/ashmoday/loans/exception/OperationNotPermittedException.java
+++ src/main/java/com/ashmoday/loans/exception/OperationNotPermittedException.java
@@ -1,7 +1,9 @@
 package com.ashmoday.loans.exception;
 
 public class OperationNotPermittedException extends RuntimeException {
+
   public OperationNotPermittedException(String msg) {
     super(msg);
   }
+
 }
