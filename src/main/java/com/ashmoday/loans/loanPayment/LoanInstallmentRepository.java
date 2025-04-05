425b18a15d007b0ab6be6ede815c06b1411fd48e
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-04-04 22:53:31 -0300

    Added findByLoanAndStatus to JPA

--- src/main/java/com/ashmoday/loans/loanPayment/LoanInstallmentRepository.java
+++ src/main/java/com/ashmoday/loans/loanPayment/LoanInstallmentRepository.java
@@ -1,7 +1,11 @@
 package com.ashmoday.loans.loanPayment;
 
+import com.ashmoday.loans.loan.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface LoanInstallmentRepository extends JpaRepository<LoanInstallment, Integer>, JpaSpecificationExecutor<LoanInstallment> {
+    List<LoanInstallment> findByLoanAndStatus(Loan loan, InstallmentStatus installmentStatus);
 }
