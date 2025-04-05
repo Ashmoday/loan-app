cd728d124424fc0ac642187708d0772b96e5ab3c
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-04-04 22:53:56 -0300

    payEarly added to InstallmentService

--- src/main/java/com/ashmoday/loans/loanPayment/LoanInstallmentService.java
+++ src/main/java/com/ashmoday/loans/loanPayment/LoanInstallmentService.java
@@ -1,16 +1,19 @@
 package com.ashmoday.loans.loanPayment;
 
 import com.ashmoday.loans.exception.OperationNotPermittedException;
+import com.ashmoday.loans.loan.Loan;
 import com.ashmoday.loans.loan.LoanRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class LoanInstallmentService {
     private final LoanInstallmentRepository loanInstallmentRepository;
-
+    private final LoanRepository loanRepository;
     public void payInstallment(Integer loanInstallmentId, int amount)
     {
         LoanInstallment installment = loanInstallmentRepository.findById(loanInstallmentId)
@@ -29,6 +32,30 @@ public class LoanInstallmentService {
         }
 
         loanInstallmentRepository.save(installment);
+    }
 
+    public void payEarly(Integer loanId, int paymentAmount) {
+        Loan loan = loanRepository.findById(loanId)
+                .orElseThrow(() -> new EntityNotFoundException("Loan not found"));
+
+        List<LoanInstallment> futureInstallments = loanInstallmentRepository
+                .findByLoanAndStatus(loan, InstallmentStatus.DISCOUNT_ELIGIBLE);
+
+        int discountRate = (int)(loan.getAmount() * loan.getInterestRate()) / 2;
+        double discountedPayment = paymentAmount * (1 - discountRate);
+
+        for (LoanInstallment installment : futureInstallments) {
+            if (discountedPayment <= 0) break;
+
+            double toPay = Math.min(installment.getRemainingAmount(), discountedPayment);
+            installment.setRemainingAmount(installment.getRemainingAmount() - toPay);
+
+            if (installment.getRemainingAmount() <= 0) {
+                installment.setStatus(InstallmentStatus.PAID);
+            }
+
+            installmentRepository.save(installment);
+            discountedPayment -= toPay;
+        }
     }
 }
