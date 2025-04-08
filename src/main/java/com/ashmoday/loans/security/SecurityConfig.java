b59b6fb40b3c50caf8f42f0f01dc1d54ca0de27e
Ashmoday <101373297+Ashmoday@users.noreply.github.com>
2025-04-08 16:57:33 -0300

    Added config for swagger

--- src/main/java/com/ashmoday/loans/security/SecurityConfig.java
+++ src/main/java/com/ashmoday/loans/security/SecurityConfig.java
@@ -30,7 +30,17 @@ public class SecurityConfig {
         http.cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize.requestMatchers(
-                                        "/auth/**"
+                                        "/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
